57ffd745b4bd7ab02a42a21ad5300f4e55bfe4a2
Daniel Santana <daniel.santana@luftblick.at>
2017-11-29 20:14:06 +0000

    Test

--- pcbasic_brewer/pcbasic/__main__.py
+++
@@ -1,9 +0,0 @@
-"""
-PC-BASIC - GW-BASIC/BASICA/Cartridge BASIC compatible interpreter
-
-(c) 2013, 2014, 2015, 2016 Rob Hagemans
-This file is released under the GNU GPL version 3 or later.
-"""
-
-from .main import main
-main()
