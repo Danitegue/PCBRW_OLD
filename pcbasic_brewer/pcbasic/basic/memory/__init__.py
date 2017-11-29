2e92a2a201090a79a0d96309a370142e24904a64
Daniel Santana <daniel.santana@luftblick.at>
2017-11-29 20:14:06 +0000

    Test

--- pcbasic_brewer/pcbasic/basic/memory/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-PC-BASIC - memory package
-Data segment
-
-(c) 2013, 2014, 2015, 2016 Rob Hagemans
-This file is released under the GNU GPL version 3 or later.
-"""
-
-from .memory import *
--- pcbasic_brewer/pcbasic/basic/parser/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-PC-BASIC - parser package
-Parser
-
-(c) 2013, 2014, 2015, 2016 Rob Hagemans
-This file is released under the GNU GPL version 3 or later.
-"""
-
-from .statements import *
